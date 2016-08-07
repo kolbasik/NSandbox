8a468d8cceca82db516f72e643318a0c0eae790a
kolbasik <sergei.kolbasin@gmail.com>
2016-08-07 17:04:43 +0200

    added tests project

---
+++ src/Sandbox.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("kolbasik.NSandbox.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("83976397-4acd-4151-aeeb-b4c092c6962a")]
---
+++ src/Sandbox.Tests/SandboxTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace kolbasik.NSandbox.Tests
+{
+    public sealed class SandboxTests
+    {
+        [Fact]
+        public void Test()
+        {
+            // arrange
+            var sandbox = new Sandbox();
+
+            // assert
+            Assert.NotNull(sandbox);
+        }
+    }
+}
